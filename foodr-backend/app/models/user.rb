8598442867fd56da56408a90c0c94e9f60d57b12
Victoria <vic.chen128@gmail.com>
2017-07-29 16:14:52 -0700

    Add validation to require password and email. Add method to return average grade as 2 decimal places

--- foodr-backend/app/models/user.rb
+++ foodr-backend/app/models/user.rb
@@ -5,9 +5,13 @@ class User < ApplicationRecord
 	has_many :products, through: :searches
 	has_many :ingredients, through: :products
 
+  validates :email, :password, presence: true
   validates :email, uniqueness: true
 
   def grade
-
+    numerator = self.products.pluck(:score).reduce { |sum, score| sum + score }
+    denominator = self.products.length
+    average = numerator / denominator
+    '%.1f' % average # returns 1 decimal place even for round numbers
   end
 end
