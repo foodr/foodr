37c35cae66ce2c1ffe73e91c68a546142d2ebc33
Kanan Kalelker <kykalelker@gmail.com>
2017-07-29 19:19:45 -0700

    Modify search, scan, sign in and sign up to render as buttons from the index page component instead of individual components

--- FoodrFrontend/index.ios.js
+++ FoodrFrontend/index.ios.js
@@ -16,22 +16,34 @@ import {
 
 
 export default class FoodrFrontend extends Component {
-  // constructor(){
-  //   super();
-  //   this.state = {
-  //     currentPage: "indexPage",
-  //     prevPage: "",
-  //     logged_in: false,
-  //     user_id: null
+  constructor(){
+    super();
+    this.state = {
+      currentPage: "indexPage",
+      prevPage: "",
+      nextPage: "",
+      searchTerm: "",
+      loggedIn: false,
+      userId: false
 
-  //   }
-  // }
+    }
+    this.handleButtonPress = this.handleButtonPress.bind(this)
+  }
+
+
+  handleButtonPress = function(pageName){
+    this.setState({currentPage: pageName})
+  }
 
 
   render() {
+
     return (
       <View style={styles.container}>
-        <IndexPage />
+        <IndexPage 
+        handleButtonPress = {this.handleButtonPress}
+        currentPage = {this.state.currentPage}
+        />
       </View>
     );
   }
@@ -40,102 +52,159 @@ export default class FoodrFrontend extends Component {
 
 
 class IndexPage extends Component {
+  constructor(props){
+    super(props)
+    this._onPressSearchButton = this._onPressSearchButton.bind(this)
+    this._onPressScanButton = this._onPressScanButton.bind(this)
+    this._onPressSignUpButton = this._onPressSignUpButton.bind(this)
+    this._onPressSignInButton = this._onPressSignInButton.bind(this)
+  }
+  
+  _onPressSearchButton(){
+    this.props.handleButtonPress("searchPage")
+  }
+
+  _onPressScanButton(){
+    this.props.handleButtonPress("cameraPage")
+  }
+
+  _onPressSignUpButton(){
+    this.props.handleButtonPress("indexPage")
+  }
+
+  _onPressSignInButton(){
+    this.props.handleButtonPress("indexPage")
+  }
+
+
+
+
+
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text>
           foodr
         </Text>
-        <ScanProduct/>
-        <SearchProduct/>
-        <SignUp/>
-        <SignIn/>
-
-      </View>
-    );
-  }
-}
-
-
-class ScanProduct extends Component {
-  _onPressButton(){
-    {Alert.alert('Taking you to the camera in just a minute..')}
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
         <Button 
-        onPress={this._onPressButton}
-        title="Scan Product"
-        color="blue"
-
+          title="Search Product"
+          onPress={this._onPressSearchButton}
+          color="green"
         />
-
-      </View>
-    );
-  }
-}
-
-class SearchProduct extends Component {
-  _onPressButton(){
-    {Alert.alert('Enter the product name or upc')}
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
         <Button 
-        onPress={this._onPressButton}
-        title="Search Product"
-        color="green"
-        />
-
-      </View>
-    );
-  }
-}
-
-class SignUp extends Component {
-  _onPressButton(){
-    {Alert.alert('Register with your email')}
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
+          title="Scan Product"
+          onPress={this._onPressScanButton}
+          color="blue"
+          />
         <Button 
-        onPress={this._onPressButton}
+        onPress={this._onPressSignUpButton}
         title="Sign Up"
         color="purple"
         />
-
-      </View>
-    );
-  }
-}
-
-class SignIn extends Component {
-  _onPressButton(){
-    {Alert.alert('You are not logged in yet')}
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-      <Text>
-        Already have an account?  
-      </Text>
-      <Button 
-        onPress={this._onPressButton}
+        <Button 
+        onPress={this._onPressSignInButton}
         title="Sign In"
-        color="green"
-        />
+        color="brown"
+        /> 
 
       </View>
     );
   }
 }
+
+
+// class SearchProduct extends Component {
+//   _onPressButton(){
+//     this.props.currentPage = "searchPage"
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Button 
+//         onPress={this._onPressButton}
+//         title="Search Product"
+//         color="green"
+//         />
+
+//       </View>
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+// class ScanProduct extends Component {
+//   _onPressButton(){
+//     this.props.currentPage = "cameraPage"
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Button 
+//         onPress={this._onPressButton}
+//         title="Scan Product"
+//         color="blue"
+
+//         />
+
+//       </View>
+//     );
+//   }
+// }
+
+
+
+
+// class SignUp extends Component {
+//   _onPressButton(){
+//     this.props.currentPage = "signupPage"
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Button 
+//         onPress={this._onPressButton}
+//         title="Sign Up"
+//         color="purple"
+//         />
+
+//       </View>
+//     );
+//   }
+// }
+
+// class SignIn extends Component {
+//   _onPressButton(){
+//     this.props.currentPage = "signinPage"
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//       <Text>
+//         Already have an account?  
+//       </Text>
+//       <Button 
+//         onPress={this._onPressButton}
+//         title="Sign In"
+//         color="green"
+//         />
+
+//       </View>
+//     );
+//   }
+// }
 
 
 
