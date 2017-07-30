ce459561d36f01759343852d4dcb5cbbcc9aa442
Victoria <vic.chen128@gmail.com>
2017-07-30 11:18:55 -0700

    Template with camera not installed.

--- FoodrFrontend/index.ios.js
+++ FoodrFrontend/index.ios.js
@@ -1,35 +1,275 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
+  Button,
+  ActivityIndicator
 } from 'react-native';
+// import Camera from 'react-native-camera';
+
+// PARENT
 
 export default class FoodrFrontend extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentPage: 'IndexPage',
+      previousPage: 'DefaultPage',
+      foundProduct: {},
+      loggedIn: false,
+      userId: false,
+      searchTerm: ''
+    }
+    this.updateCurrentPage = this.updateCurrentPage.bind(this)
+    this.searchProduct = this.searchProduct.bind(this)
+  }
+
+  searchProduct(upc) {
+    this.updateCurrentPage('SearchingPage')
+
+    fetch('https://dbc-foodr-api.herokuapp.com/products/' + upc + "?user_id=" + this.state.userId)
+    // fetch('http://localhost:3000/products/' + upc + "?user_id=" + this.state.userId)
+    .then((data) => data.json())
+    .then((jsonData) => {
+      this.setState({ foundProduct: jsonData })
+      if (this.state.foundProduct.found) {
+        this.updateCurrentPage('ProductPage')
+      } else {
+        this.updateCurrentPage('NoResultsPage')
+      }
+    });
+  }
+
+  updateCurrentPage(pageName) {
+    this.setState({currentPage: pageName})
+  }
+
   render() {
-    return (
+    switch(this.state.currentPage) {
+      case 'IndexPage':
+        return(
+          <IndexPage />
+        )
+      case 'SearchPage':
+        return(
+          <SearchPage />
+        )
+      case 'CameraPage':
+        return(
+          <CameraPage
+            searchProduct = {this.searchProduct}
+          />
+        )
+      case 'ProductPage':
+        return(
+          <ProductPage
+            foundProduct = {this.state.foundProduct}
+            updateCurrentPage = {this.updateCurrentPage}
+          />
+        )
+      case 'IngredientPage':
+        return(
+          <IngredientPage />
+        )
+      case 'NoResultsPage':
+        return(
+          <NoResultsPage
+            updateCurrentPage = {this.updateCurrentPage}
+          />
+        )
+      case 'SearchingPage':
+        return(
+          <SearchingPage />
+        )
+      default:
+        return(
+          <DefaultPage />
+        )
+    }
+  }
+}
+
+// CHILDREN
+
+// XANDER
+
+class LayoutPage extends Component {
+  render() {
+    return(
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to FOOOOOODR!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>Layout Page</Text>
+      </View>
+    )
+  }
+}
+
+// VICTORIA
+
+class CameraPage extends Component {
+  constructor() {
+    super()
+    this.onBarCodeRead = this.onBarCodeRead.bind(this)
+    this.existingItem = this.existingItem.bind(this)
+    this.nonExistingItem = this.nonExistingItem.bind(this)
+  }
+
+  onBarCodeRead(e) {
+    this.props.searchProduct(e.data)
+  }
+
+  // for testing
+  existingItem() {
+    this.props.searchProduct('03077504')
+  }
+
+  nonExistingItem() {
+    this.props.searchProduct('asdf')
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Place Barcode in View</Text>
+        <Camera
+          ref={(cam) => {this.camera = cam;}}
+          style={styles.preview}
+          onBarCodeRead = {this.onBarCodeRead}
+          aspect={Camera.constants.Aspect.fill}>
+        </Camera>
+        <Text style={styles.instructions}>The camera will automatically detect when a barcode is present</Text>
+
+
+        <Text style={styles.capture} onPress={this.existingItem}>[EXISTING ITEM]</Text>
+        <Text style={styles.capture} onPress={this.nonExistingItem}>[NONEXISTING ITEM]</Text>
       </View>
     );
   }
 }
+
+// TIFF
+
+class ProductPage extends Component {
+  constructor() {
+    super()
+    this.goToCamera = this.goToCamera.bind(this)
+    this.goToSearch = this.goToSearch.bind(this)
+  }
+
+  goToCamera () {
+    this.props.updateCurrentPage('CameraPage')
+  }
+
+  goToSearch() {
+    this.props .updateCurrentPage('SearchPage')
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome} >Product Page</Text>
+        <Text>{this.props.foundProduct.product.name}</Text>
+        <Button
+          onPress={this.goToCamera}
+          title="Scan Another Item"
+        />
+      </View>
+    )
+  }
+}
+
+// KANAN
+
+class NoResultsPage extends Component {
+  constructor() {
+    super()
+    this.goToCamera = this.goToCamera.bind(this)
+    this.goToSearch = this.goToSearch.bind(this)
+  }
+
+  goToCamera () {
+    this.props.updateCurrentPage('CameraPage')
+  }
+
+  goToSearch() {
+    this.props .updateCurrentPage('SearchPage')
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>No Products Found</Text>
+        <Text>Sorry we do not have that item in our database{"\n\n"}</Text>
+        <Button
+            onPress={this.goToCamera}
+            title="Scan Another Item"
+          />
+        <Text>or</Text>
+        <Button
+            onPress={this.goToSearch}
+            title="Search for an Item"
+          />
+      </View>
+    )
+  }
+}
+
+// KANAN
+
+class SearchPage extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text>Search Page</Text>
+      </View>
+    );
+  }
+}
+
+// KANAN
+
+class IndexPage extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text>Index Page</Text>
+      </View>
+    );
+  }
+}
+
+// DONE
+
+class SearchingPage extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <ActivityIndicator
+          animating = {true}
+          size = 'large'
+          style={styles.activityIndicator}
+        />
+        <Text>Searching...</Text>
+      </View>
+    );
+  }
+}
+
+// FOR TESTING
+
+class DefaultPage extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Default Page</Text>
+        <Text>The case statement hit default</Text>
+      </View>
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -46,8 +286,24 @@ const styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    margin: 10,
   },
+  preview: {
+    height: 300,
+    width: Dimensions.get('window').width
+  },
+  activityIndicator: {
+    marginBottom: 20,
+  },
+  // for testing
+  capture: {
+    flex: 0,
+    backgroundColor: 'lightblue',
+    borderRadius: 5,
+    padding: 10,
+    margin: 5
+  },
+
 });
 
 AppRegistry.registerComponent('FoodrFrontend', () => FoodrFrontend);
