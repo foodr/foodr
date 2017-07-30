94a386af972331a691a98a19cf2a619d9fa51543
Kanan Kalelker <kykalelker@gmail.com>
2017-07-30 12:17:19 -0700

    clean up template in index.ios.js file

--- FoodrFrontend/index.ios.js
+++ FoodrFrontend/index.ios.js
@@ -1,136 +1,248 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
+  Dimensions,
   Button,
-  Alert
+  ActivityIndicator
 } from 'react-native';
+// import Camera from 'react-native-camera';
 
+// PARENT
 
 export default class FoodrFrontend extends Component {
-  constructor(){
-    super();
+  constructor() {
+    super()
     this.state = {
-      currentPage: "indexPage",
-      prevPage: "",
-      nextPage: "",
-      searchTerm: "",
+      currentPage: 'IndexPage',
+      previousPage: 'DefaultPage',
+      foundProduct: {},
       loggedIn: false,
-      userId: false
-
+      userId: false,
+      searchTerm: ''
     }
     this.updateCurrentPage = this.updateCurrentPage.bind(this)
+    this.updateSearchTerm = this.updateSearchTerm.bind(this)
+    this.searchProduct = this.searchProduct.bind(this)
   }
 
 
-  updateCurrentPage = function(pageName){
-    console.log("Pagename" + pageName)
+  updateCurrentPage(pageName) {
     this.setState({currentPage: pageName})
-
   }
 
+  updateSearchTerm(searchTerm){
+    this.setState({searchTerm: searchTerm})
+  }
+
+  searchProduct(upc) {
+    this.updateCurrentPage('SearchingPage')
+
+    fetch('https://dbc-foodr-api.herokuapp.com/products/' + upc + "?user_id=" + this.state.userId)
+    // fetch('http://localhost:3000/products/' + upc + "?user_id=" + this.state.userId)
+    .then((data) => data.json())
+    .then((jsonData) => {
+      this.setState({ foundProduct: jsonData })
+      if (this.state.foundProduct.found) {
+        this.updateCurrentPage('ProductPage')
+      } else {
+        this.updateCurrentPage('NoResultsPage')
+      }
+    });
+  }
 
   render() {
-
-      switch (this.state.currentPage) {
-
-        case 'indexPage': return (
+    switch(this.state.currentPage) {
+      case 'IndexPage':
+        return(
           <IndexPage 
           updateCurrentPage = {this.updateCurrentPage}
-          currentPage = {this.state.currentPage}
-        />)
-
-       case 'searchPage': return (
+          currentPage = {this.state.currentPage}/>
+        )
+      case 'SearchPage':
+        return(
           <SearchPage
-        updateCurrentPage = {this.updateCurrentPage}
-        currentPage = {this.state.currentPage}
-        />)
+          updateCurrentPage = {this.updateCurrentPage}
+          currentPage = {this.state.currentPage}
+          searchTerm = {this.state.searchTerm}
+          updateSearchTerm = {this.updateSearchTerm} />
+        )
+      case 'CameraPage':
+        return(
+          <CameraPage
+            searchProduct = {this.searchProduct}
+          />
+        )
+      case 'ProductPage':
+        return(
+          <ProductPage
+            foundProduct = {this.state.foundProduct}
+            updateCurrentPage = {this.updateCurrentPage}
+          />
+        )
+      case 'IngredientPage':
+        return(
+          <IngredientPage />
+        )
+      case 'NoResultsPage':
+        return(
+          <NoResultsPage
+            updateCurrentPage = {this.updateCurrentPage}
+          />
+        )
+      case 'SearchingPage':
+        return(
+          <SearchingPage />
+        )
+      default:
+        return(
+          <DefaultPage />
+        )
+    }
+  }
 
-        default: return (
-          <UserPage/>)
+}
 
-      }
+// CHILDREN
 
+// XANDER
+
+// class LayoutPage extends Component {
+//   render() {
+//     return(
+//       <View style={styles.container}>
+//         <Text>Layout Page</Text>
+//       </View>
+//     )
+//   }
+// }
+
+
+// VICTORIA
+
+// class CameraPage extends Component {
+//   constructor() {
+//     super()
+//     this.onBarCodeRead = this.onBarCodeRead.bind(this)
+//     this.existingItem = this.existingItem.bind(this)
+//     this.nonExistingItem = this.nonExistingItem.bind(this)
+//   }
+
+//   onBarCodeRead(e) {
+//     this.props.searchProduct(e.data)
+//   }
+
+//   // for testing
+//   existingItem() {
+//     this.props.searchProduct('03077504')
+//   }
+
+//   nonExistingItem() {
+//     this.props.searchProduct('asdf')
+//   }
+
+//   render() {
+//     return(
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>Place Barcode in View</Text>
+//         <Camera
+//           ref={(cam) => {this.camera = cam;}}
+//           style={styles.preview}
+//           onBarCodeRead = {this.onBarCodeRead}
+//           aspect={Camera.constants.Aspect.fill}>
+//         </Camera>
+//         <Text style={styles.instructions}>The camera will automatically detect when a barcode is present</Text>
+
+
+//         <Text style={styles.capture} onPress={this.existingItem}>[EXISTING ITEM]</Text>
+//         <Text style={styles.capture} onPress={this.nonExistingItem}>[NONEXISTING ITEM]</Text>
+//       </View>
+//     );
+//   }
+// }
+
+
+// TIFF
+
+class ProductPage extends Component {
+  constructor() {
+    super()
+    this.goToCamera = this.goToCamera.bind(this)
+    this.goToSearch = this.goToSearch.bind(this)
+  }
+
+  goToCamera () {
+    this.props.updateCurrentPage('CameraPage')
+  }
+
+  goToSearch() {
+    this.props .updateCurrentPage('SearchPage')
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome} >Product Page</Text>
+        <Text>{this.props.foundProduct.product.name}</Text>
+        <Button
+          onPress={this.goToCamera}
+          title="Scan Another Item"
+        />
+      </View>
+    )
   }
 }
 
+// KANAN
 
-
-class IndexPage extends Component {
+class NoResultsPage extends Component {
   constructor(props){
     super(props)
-    this._onPressSearchButton = this._onPressSearchButton.bind(this)
-    this._onPressScanButton = this._onPressScanButton.bind(this)
-    this._onPressSignUpButton = this._onPressSignUpButton.bind(this)
-    this._onPressSignInButton = this._onPressSignInButton.bind(this)
+    this.searchAgain = this.searchAgain.bind(this)
+    this.scanAgain = this.scanAgain.bind(this)
   }
-  
-  _onPressSearchButton(){
+
+  searchAgain(){
     this.props.updateCurrentPage("searchPage")
   }
 
-  _onPressScanButton(){
+  scanAgain(){
     this.props.updateCurrentPage("cameraPage")
-  }
-
-  _onPressSignUpButton(){
-    this.props.updateCurrentPage("indexPage")
-  }
-
-  _onPressSignInButton(){
-    this.props.updateCurrentPage("indexPage")
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          foodr
-        </Text>
+        <Text style={styles.welcome}>No Products Found</Text>
+        <Text> Would you like to try another product?</Text>
         <Button 
-          title="Scan Product"
-          onPress={this._onPressScanButton}
-          color="blue"
-          />
-          <Button 
-          title="Search Product"
-          onPress={this._onPressSearchButton}
-          color="green"
+         title="Scan Another Product"
+         onPress={this.scanAgain}
+         color="blue"
         />
+        <Text>or</Text>
         <Button 
-        onPress={this._onPressSignUpButton}
-        title="Sign Up"
-        color="purple"
+         title="Search Product"
+         onPress={this.searchAgain}
+         color="green"
         />
-        <Button 
-        onPress={this._onPressSignInButton}
-        title="Sign In"
-        color="brown"
-        /> 
-
       </View>
     );
   }
 }
 
 
-
 class SearchPage extends Component {
   constructor(props){
     super(props)
-    this._onPressSearchButton = this._onPressSearchButton(this)
+    this._onPressSearchWord = this._onPressSearchWord(this)
   }
 
-  _onPressSearchButton(){
+  _onPressSearchWord(){
     this.props.updateCurrentPage("noResultsPage")
+    this.props.updateSearchTerm("something")
   }
 
   render() {
@@ -146,37 +258,75 @@ class SearchPage extends Component {
 }
 
 
-class UserPage extends Component {
+class IndexPage extends Component {
+  constructor(props){
+    super(props)
+    this._onPressSearchButton = this._onPressSearchButton.bind(this)
+    this._onPressScanButton = this._onPressScanButton.bind(this)
+    this._onPressSignUpButton = this._onPressSignUpButton.bind(this)
+    this._onPressSignInButton = this._onPressSignInButton.bind(this)
+  }
   
+  _onPressSearchButton(){
+    this.props.updateCurrentPage("SearchPage")
+  }
+
+  _onPressScanButton(){
+    this.props.updateCurrentPage("CameraPage")
+  }
+
+  _onPressSignUpButton(){
+    this.props.updateCurrentPage("IndexPage")
+  }
+
+  _onPressSignInButton(){
+    this.props.updateCurrentPage("IndexPage")
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>
-          User Profile Page
+          foodr
         </Text>
+        <Button title="Scan Product" onPress={this._onPressScanButton} color="blue" />
+        <Button title="Search Product" onPress={this._onPressSearchButton} color="green" />
+        <Button onPress={this._onPressSignUpButton} title="Sign Up" color="purple" />
+        <Button onPress={this._onPressSignInButton} title="Sign In" color="brown"/> 
       </View>
     );
   }
 }
 
+// DONE
 
+class SearchingPage extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <ActivityIndicator
+          animating = {true}
+          size = 'large'
+          style={styles.activityIndicator}
+        />
+        <Text>Searching...</Text>
+      </View>
+    );
+  }
+}
 
+// FOR TESTING
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class DefaultPage extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Default Page</Text>
+        <Text>The case statement hit default</Text>
+      </View>
+    );
+  }
+}
 
 
 const styles = StyleSheet.create({
@@ -194,7 +344,22 @@ const styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    margin: 10,
+  },
+  preview: {
+    height: 300,
+    width: Dimensions.get('window').width
+  },
+  activityIndicator: {
+    marginBottom: 20,
+  },
+  // for testing
+  capture: {
+    flex: 0,
+    backgroundColor: 'lightblue',
+    borderRadius: 5,
+    padding: 10,
+    margin: 5
   },
 });
 
