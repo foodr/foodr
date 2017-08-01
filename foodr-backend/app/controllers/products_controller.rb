2e6ad3cb12d3dc1085325c064d97877186b0b99f
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-07-31 20:15:33 -0700

    Add textacular gem for fuzzy search in ActiveRecord, begin implementing backend logic to handle returning possible matches to search

--- foodr-backend/app/controllers/products_controller.rb
+++ foodr-backend/app/controllers/products_controller.rb
@@ -2,6 +2,9 @@ class ProductsController < ApplicationController
   def search
     search_term = params[:search_term]
     product = Product.find_by(upc: search_term) || Product.find_by(name: search_term)
+    # Does a fuzzy search if exact product name wasn't found
+    possible_matches = Product.fuzzy_search(name: search_term) if !product
+    p possible_matches
     if product
       if params[:user_id]
         search = Search.create(user_id: params[:user_id], product_id: product.id, is_saved: false)
@@ -11,6 +14,11 @@ class ProductsController < ApplicationController
         product: product,
         ingredients: product.ingredients,
         search: search
+      }.to_json
+    elsif possible_matches
+      render json: {
+        found: 'maybe',
+        matches: possible_matches
       }.to_json
     else
       render json: { found: false }.to_json
