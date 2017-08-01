e851b704aff68abec17c4010e534c5a23a0d2b26
Victoria <vic.chen128@gmail.com>
2017-08-01 16:09:40 -0700

    Change login route and method name from authenticate to login. Add authenticate call and error handling to login method.

--- foodr-backend/config/routes.rb
+++ foodr-backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'products/:search_term', to: 'products#search'
   get 'ingredients/:id', to: 'ingredients#show'
   get 'users/profile/:id', to: 'users#show'
-  get 'users/authenticate' , to: 'users#authenticate'
+  get 'users/login' , to: 'users#login'
   post 'users/', to: 'users#create'
   post 'searches/:id/save', to: 'searches#save'
 end
