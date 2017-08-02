9ef8e82372865abc3e5bc75f4e844a4bcc621912
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-08-02 13:45:32 -0700

    Refactor fuzzy search results to only return product name, id

--- foodr-backend/app/controllers/products_controller.rb
+++ foodr-backend/app/controllers/products_controller.rb
@@ -20,10 +20,6 @@ class ProductsController < ApplicationController
   def search_name
     search_term = params[:search_term]
     product = Product.find_by(name: search_term)
-    # Does a fuzzy search if exact product name wasn't found
-    if !product
-      possible_matches = Product.fuzzy_search(name: search_term)
-    end
     if product
       if params[:user_id]
         search = Search.create(user_id: params[:user_id], product_id: product.id, is_saved: false)
@@ -33,16 +29,33 @@ class ProductsController < ApplicationController
         product: product,
         ingredients: product.ingredients,
         search: search
-      }.to_json
-    elsif possible_matches.any?
-      render json: {
-        matches: possible_matches
-      }.to_json
-    else
-      render json: {
-        found: false,
-        matches: []
-      }.to_json
+      }.to_json and return
     end
+
+    # Does a fuzzy search if exact product name wasn't found
+    # matches is an array of hashes, each containing a product name and id
+    matches = fuzzy_search_name(search_term)
+    if matches
+      render json: {
+        matches: matches
+      }.to_json and return
+    end
+
+    # Returns found: false if both exact match and fuzzy search yielded no results
+    render json: {
+      found: false
+    }.to_json
+  end
+
+
+  private
+
+  def fuzzy_search_name(search_term)
+    possible_matches = Product.fuzzy_search(name: search_term)
+    matches = []
+    possible_matches.each do |product|
+      matches << {id: product.id, name: product.name}
+    end
+    matches.any? ? matches : nil
   end
 end
