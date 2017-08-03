e690c58d16924a9edf4d1449f35d1dfb8837fc61
Victoria <vic.chen128@gmail.com>
2017-08-03 11:27:03 -0700

    Change spacing for readability

--- foodr-backend/config/routes.rb
+++ foodr-backend/config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get 'products/upc/:search_term', to: 'products#search_upc'
 
   get 'ingredients/:id', to: 'ingredients#show'
+
   get 'users/profile/:id', to: 'users#show'
   get 'users/login' , to: 'users#login'
   post 'users/', to: 'users#create'
+
   post 'searches/:id/save', to: 'searches#save'
   get 'searches/:id', to: 'searches#show'
 end
